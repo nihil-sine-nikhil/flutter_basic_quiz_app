7cc0eefbc419c12271b0acb7b37105c58de32c0c
nIkHiL <zero.zero619.ns@gmail.com>
2023-07-09 23:04:59 +0530

    added shuffle method

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -3,4 +3,9 @@ class QuizQuestion {
 
   final String text;
   final List<String> answers;
+  List<String> getShuffledAnswers() {
+    var newList = List.of(answers);
+    newList.shuffle();
+    return newList;
+  }
 }
